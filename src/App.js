8a8548c3ffd0522af93419dacc7c8ed7dc1e7f4c
Kimberly Ivie <ktbt02@yahoo.com>
2020-03-14 16:01:49 -0500

    set up react repo and added in a todo list funtion

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useRef } from 'react';
+import TodoList from './TodoList';
+
+function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    console.log(name)
+  }
+
+  return (
+    <>
+      <TodoList todos={todos}/>
+      <input ref ={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear complete</button>
+      <div>0 left to do</div>
+    </>
+  )
+}
+
+export default App;
