8c259441110f67a8528664a1cf408d6eafd4d168
Kimberly Ivie <ktbt02@yahoo.com>
2020-03-14 16:12:26 -0500

    can now add todos

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
-    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: 1, name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
   }
 
   return (
